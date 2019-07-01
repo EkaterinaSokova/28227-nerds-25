f2cdfc37f4012446cb8a0e9fad8bdfa7cd19a86b
EkaterinaSokova <50383275+EkaterinaSokova@users.noreply.github.com>
2019-07-02 00:50:45 +0300

    Защита

---
+++ js/popup.js
@@ -0,0 +1,58 @@
+var link = document.querySelector(".button-contact");
+
+var popup = document.querySelector(".modal-letter");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var username = popup.querySelector("[name=username]");
+var password = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("username");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+
+  if (storage) {
+    username.value = storage;
+    email.focus();
+  } else {
+    username.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!username.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("username", username.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
